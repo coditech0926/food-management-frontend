ee3f4376b98dc3e95d52b95434a057feba6bf01f
Dragon926 <coditech.super926@gmail.com>
2023-10-20 09:34:50 +0900

    update: layout component

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Spinner } from '../components/Spinner';
 import { usePartyContext } from '../context';
 import { Header } from './Header';
 
 export const Layout = ({ children }: any) => {
   const [state]: any = usePartyContext();
+  const navigate: any = useNavigate();
+
+  useEffect(() => {
+    if (!state.token) {
+      navigate('/login');
+    }
+    // eslint-disable-next-line
+  }, [state.token]);
 
   return (
     <>
