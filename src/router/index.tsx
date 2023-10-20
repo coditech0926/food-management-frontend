e41ad6ff074ae6db78e67dc4123d215b1650cac7
Dragon926 <coditech.super926@gmail.com>
2023-10-20 09:13:00 +0900

    feat: add new routers

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,23 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Layout } from '../layout';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { Category } from '../pages/Category';
 import { Filter } from '../pages/Filter';
 import { Meal } from '../pages/Meal';
 import { Ingredient } from '../pages/Ingredient';
+import { Login } from '../pages/Login';
+import { Register } from '../pages/Register';
+import { AuthLayout } from '../components/AuthLayout';
 
 export const Router = () => {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/filter" element={<Filter />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/meals/:id" element={<Meal />} />
-          <Route path="/ingredients/:name" element={<Ingredient />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+        <Route path="/home" element={<Home />} />
+        <Route path="/filter" element={<Filter />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/meals/:id" element={<Meal />} />
+        <Route path="/ingredients/:name" element={<Ingredient />} />
+      </Routes>
+    </BrowserRouter >
   );
 };
